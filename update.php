563f3f2327114dc93b16d5cbfd9ee9cd2858ebad
p.manikanta swamy <swamy81293@gmail.com>
2016-02-20 19:34:35 +0530

    remove the place holder contents in update.php and sql content also

--- update.php
+++ update.php
@@ -1,39 +1,4 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) session_start();
-include_once 'dbconn.php';
-$uid=$_SESSION['user'];
-
-$lid=$_GET['lid'];
-$lwid=$_GET['lwid'];
-
-$record=mysql_query("select * from table_listing where listing_id='$lid'");
-$details=mysql_fetch_assoc($record);
-$name=$details['listing_name'];
-$add=$details['listing_address'];
-$amount=$details['listing_price'];
-
-if(isset($_POST['update']))
-{
-	$listname =($_POST['lname']);
-	$address =($_POST['address']);
-	$price =($_POST['price']);
-	 
-	
-	
-		if(mysql_query("update table_listing set listing_name='$listname',listing_address='$address',listing_price='$price' where listing_id='$lid'"))
-				{
-				echo "<script>alert('Your listing details is successfully Updateded !');
- 						window.location.href='viewlistings.php'; </script>";
-		        }
-
-				else
-					{
-					echo "<script>alert('Your updation failed Plese verifye details !');
- 						window.location.href='update.php'; </script>";
-					}
-
-}
-?>
+<?php include 'listing.php';?>
   <html >
 		<head>
 			<title>Update Listing</title>
@@ -43,16 +8,17 @@ if(isset($_POST['update']))
 		<div id="header"><h1 align="center">Squaredoor.com</h1></div>
 			<div  align="center"><?php include 'links.php';?></div>
 	<div id="login-form" align="center"><h2>Update Listings</h2>
+	<div><?php listing::Update();?></div>
 		<form method="post">
 			<table width="25%">
 				<tr>
-					<td><input type="text" name="lname" value=<?php echo $name;?>  /></td>
+					<td><input type="text" name="lname" /></td>
 				</tr>
 				<tr>
-					<td><input type="text" name="address" value=<?php echo $add;?>  /></td>
+					<td><input type="text" name="address" /></td>
 				</tr>
 				<tr>
-					<td><input type="text" name="price" value=<?php echo $amount;?>  /></td>
+					<td><input type="text" name="price"  /></td>
 				</tr>
 				<tr>
 					<td><button type="submit" name="update">Update List</button></td>
