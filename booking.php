dc753fa38838d18d01c4f3cf87d5ace3e64b499f
p.manikanta swamy <swamy81293@gmail.com>
2016-02-20 19:29:34 +0530

    written content of sql action related to booking section .

--- booking.php
+++ booking.php
@@ -1,22 +1,57 @@
 <?php 
-if (session_status() === PHP_SESSION_NONE) session_start();
-   include_once 'dbconn.php';
-   $uid=$_SESSION['user'];
-  
-   $lid=$_GET['lid'];
-   $records=mysql_query("select*from table_listing where listing_id='$lid'");
-   $listdetails=mysql_fetch_assoc($records);
-   $bprice=$listdetails['listing_price'];
+	if(isset($_POST['var']))
+	{
+		booking::Create($_POST['id']);
+	}	//Booking::Create();
 
- if(mysql_query("insert into table_booking(booking_listing_id,booking_user_id,booking_amount)values($lid,$uid,$bprice)"))
- 	{
- 		echo "<script>  alert('Booking successfully completed !');
- 				window.location.href='viewindex.php'; </script>";
- 	}
- 	else
- 	{
- 		echo "<script>alert('Booking is failed !');
- 				window.location.href='viewindex.php'; </script>";
-    }
+	class booking
+	{
+
+		 public static function Create($lid)
+		{
+		if (session_status() === PHP_SESSION_NONE) session_start();
+   				include_once 'dbconn.php';
+  					
+   				$uid=$_SESSION['user'];
+   				$records=mysql_query("select * from table_listing where listing_id='$lid'");
+   				$listdetails=mysql_fetch_assoc($records);
+   				$bprice=$listdetails['listing_price'];
+
+ 			if(mysql_query("insert into table_booking(booking_listing_id,booking_user_id,booking_amount)values($lid,$uid,$bprice)"))
+ 				{
+ 					echo "1";
+ 				}
+ 				else
+ 				{
+ 					echo "0";
+   				}
+
+    		
+			}
+			
+			public static function display()
+			{
+				if (session_status() === PHP_SESSION_NONE) session_start();
+				include 'dbconn.php';
+				
+				$uid=$_SESSION['user'];
+				$sql="select* from table_booking where booking_user_id='$uid'order by booking_id desc";
+				$records=mysql_query($sql);
+				
+				while ($table_booking=mysql_fetch_assoc($records))
+				{
+					echo "<table >";
+					echo "<tr>"."<td>"."Booking_Id:"."</td><td>".$table_booking['booking_id']."</td></tr>";
+					echo "<tr>"."<td>"."Booking_Listing_Id:"."</td><td>".$table_booking['booking_listing_id']."</td></tr>";
+					echo "<tr>"."<td>"."Booking_User_Id:"."</td><td>".$table_booking['booking_user_id']."</td></tr>";
+					echo "<tr>"."<td>". "Booking_Amount:"."</td><td>".$table_booking['booking_amount']."</td></tr>";
+					echo "---------------------------------------";
+				}
+					echo"</table>";
+			}
+		
+	}
+
+
 ?>
     		
